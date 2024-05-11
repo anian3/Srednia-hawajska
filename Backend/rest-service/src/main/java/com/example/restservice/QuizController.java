39460b42f00f00c9f31ebcaddc45ecc7e3ef2dba
XT60 <89086129+XT60@users.noreply.github.com>
2024-05-11 23:46:54 +0200

    java prettier on the backend

--- Backend/rest-service/src/main/java/com/example/restservice/QuizController.java
+++ Backend/rest-service/src/main/java/com/example/restservice/QuizController.java
@@ -3,7 +3,6 @@ package com.example.restservice;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QuizController {
 
-	private static final String QUIZZES_DIR_PATH = "./quizzes/";
+    private static final String QUIZZES_DIR_PATH = "./quizzes/";
 
-	//TODO: This is a stub.
-	@GetMapping("/quiz")
-	public String greeting(@RequestParam(value = "quizName", defaultValue = "defaultQuiz.txt") String name) throws IOException {
-		return Files.readString(Paths.get(QUIZZES_DIR_PATH.concat(name)));
-	}
+    //TODO: This is a stub.
+    @GetMapping("/quiz")
+    public String greeting(@RequestParam(value = "quizName", defaultValue = "defaultQuiz.txt") String name)
+        throws IOException {
+        return Files.readString(Paths.get(QUIZZES_DIR_PATH.concat(name)));
+    }
 }
