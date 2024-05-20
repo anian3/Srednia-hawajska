a03b7fa5f156df7c024fab67f43494a84bfe03a0
Mpieta <maps321.mp@gmail.com>
2024-05-20 13:30:39 +0200

    Initial score component

---
+++ Frontend/src/components/QuizResult.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ScoreCard from './ScoreCard';
+import MistakesList from './MistakesList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface QuizResultProps {
+    score: number;
+    mistakes: string[];
+}
+
+const QuizResult: React.FC<QuizResultProps> = ({ score, mistakes }) => {
+    return (
+        <div style={{ width: '16rem', margin: 'auto', marginLeft: '60%' }}>
+            <ScoreCard score={score} />
+            {mistakes.length > 0 && <MistakesList mistakes={mistakes} />}
+        </div>
+    );
+};
+
+export default QuizResult;
--- Frontend/src/pages/QuizPage/QuizPage.tsx
+++ Frontend/src/pages/QuizPage/QuizPage.tsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import CodeSmellList from "../../components/CodeSmellList";
+import QuizResult from "../../components/QuizResult";
 
 interface QuizPageProps {
     selectedQuiz: string;
 }
 
 const QuizPage = ({ selectedQuiz }: QuizPageProps) => {
+    const [submitted, setSubmitted] = useState(false);
+    const [score, setScore] = useState<number>(0);
+    const [mistakes, setMistakes] = useState<string[]>([]);
+
     const items = [
         { id: "1", label: "Option 1" },
         { id: "2", label: "Option 2" },
         { id: "3", label: "Option 3" },
     ];
+
+    const handleQuizSubmit = () => {
+        const calculatedScore = 8;
+        const quizMistakes = ["Mistake 1", "Mistake 2", "Mistake 3"];
+
+        setScore(calculatedScore);
+        setMistakes(quizMistakes);
+        setSubmitted(true);
+    };
+
+
     return (
-        <>
-            <div>QuizPage</div>
-            <div>{selectedQuiz}</div>
-            <CodeSmellList items={items}></CodeSmellList>
-        </>
+        <div className="d-flex">
+            <div style={{ flex: 1, padding: '1rem' }}>
+                <div>QuizPage</div>
+                <div>{selectedQuiz}</div>
+                <CodeSmellList items={items} />
+                {!submitted && <button onClick={handleQuizSubmit}>Submit</button>}
+            </div>
+            <div style={{ flex: 1, padding: '1rem' }}>
+                {submitted && <QuizResult score={score} mistakes={mistakes} />}
+            </div>
+        </div>
     );
 };
 
